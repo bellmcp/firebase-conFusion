8bcce9e5c7db5ea778015147fcb5d9240981050e
Wutipat Khamnuansin <bellmcp@gmail.com>
2020-09-27 23:42:14 +0700

    Transferred files from confusion repo and optimized for Firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const config = {
+  apiKey: "AIzaSyC9fiYBQ9lPzVu_t-Ifpzz3tky5N1pg2Os",
+  authDomain: "bellmcp-confusionserver.firebaseapp.com",
+  databaseURL: "https://bellmcp-confusionserver.firebaseio.com",
+  projectId: "bellmcp-confusionserver",
+  storageBucket: "bellmcp-confusionserver.appspot.com",
+  messagingSenderId: "371692228914",
+  appId: "1:371692228914:web:a39d69bb574e4c31d2872b",
+};
