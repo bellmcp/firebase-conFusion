cab2ad53c6280034e6d6a0fd641d1deb2b9147bb
Wutipat Khamnuansin <bellmcp@gmail.com>
2020-10-25 16:05:46 +0700

    Fix deploy error 2

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-var express = require("express");
-var app = express();
-var port = process.env.PORT || 3000;
-var path = require("path");
+const express = require("express");
+const path = require("path");
+const app = express();
 
-app.use(express.static("build"));
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/build/index.html"));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(port);
+app.listen(9000);
